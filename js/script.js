1c73491bf7af44e07e5311f9b141e22f54c204f3
astrum12121 <2007drin53@gmail.com>
2025-06-28 06:42:32 +0300

    Delete js/script.js

--- js/script.js
+++
@@ -1,5 +0,0 @@
-// Глобальные переменные
-let cart = JSON.parse(localStorage.getItem('cart')) || [];
-const products = [
-    // Лекарства
-    { id: 1, category: "medicines", name: "Нурофен таблетки 200мг №20", price
